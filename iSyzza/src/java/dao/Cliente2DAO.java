995dce5156ad6b13f8aaf4478d70de63437128ff
jonathan.videira <jonathan.videira@65e67a7f-7de6-4582-0608-db2226dce9a1>
2011-11-09 17:12:52 +0000

    Hibernate funcionando!!

---
+++ iSyzza/src/java/dao/Cliente2DAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.Cliente;
+import java.util.ArrayList;
+import java.util.Date;
+import org.hibernate.HibernateException;
+
+/**
+ *
+ * @author 0669105
+ */
+public class Cliente2DAO implements OperacoesDAO {
+
+    @Override
+    public boolean inserir(Object obj) {
+        Cliente cliente = (Cliente) obj;
+        h.beginTransaction();
+        try {
+            h.saveOnly(cliente);
+        } catch (HibernateException e) {
+            System.out.println("chegou aki");
+        }
+        h.endTransaction();
+        return true;
+    }
+
+    @Override
+    public ArrayList pesquisar() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean editar(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean excluir(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+   
+}
